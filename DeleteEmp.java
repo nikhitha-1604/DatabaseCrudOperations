53f88c8612a6adcd012cc9e2fcee133ac311065b
Nikhitha Vankadhari <24wh1a0568@bvrithyderabad.edu.in>
2026-04-19 20:16:19 +0530

    DeleteEmp.java for deleting the employee data
    Removed unnecessary whitespace and improved code structure.

--- DeleteEmp.java
+++ DeleteEmp.java
@@ -1,31 +1,16 @@
 import java.sql.*;
 
-
-
     public class DeleteEmp {
 
         public static void main(String []args) throws SQLException {
 
        try {
 
-         
-
            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
-
-              
-
-            
 
               Connection connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/24WH1A0568","root","1234");
 
-            
-
-        
-
-           
-
-      
-
+          
             Statement statement=connection.createStatement();
 
             int nor = statement.executeUpdate("DELETE FROM employee WHERE emp_id = 3");
@@ -34,11 +19,7 @@ import java.sql.*;
 
             ResultSet rs=statement.executeQuery("select * from employee");
 
-            
-
             while(rs.next()) {
-
-            
 
                System.out.println(
 
@@ -57,84 +38,7 @@ import java.sql.*;
            e.printStackTrace();
 
          }
-
-            
-
-        }
-
-    }
-
-
-
-
-
-
-
-
-
-package pack;
-
-import java.sql.*;
-
-
-
-    public class DeleteEmp {
-
-        public static void main(String []args) throws SQLException {
-
-       try {
-
-         
-
-           DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
-
-              
-
-            
-
-              Connection connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/24WH1A0568","root","1234");
-
-            
-
-        
-
-           
-
-      
-
-            Statement statement=connection.createStatement();
-
-            int nor = statement.executeUpdate("DELETE FROM employee WHERE emp_id = 3");
-
-            System.out.println("Rows deleted: " + nor);
-
-            ResultSet rs=statement.executeQuery("select * from employee");
-
-            
-
-            while(rs.next()) {
-
-            
-
-               System.out.println(
-
-                      rs.getInt("emp_id") + " | " +
-
-                      rs.getString("emp_name") + " | " +
-
-                      rs.getString("salary")
-
-                  );
-
-            }
-
-         }catch(SQLException e) {
-
-           e.printStackTrace();
-
-         }
-
-            
+ 
 
         }
 
@@ -143,6 +47,15 @@ import java.sql.*;
 
 
 
+
+
+
+
+
+
+     
+
+ 
 
 
 
