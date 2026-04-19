7c556ea947bffec916d0225a4cd1ba1b258f028d
Nikhitha Vankadhari <24wh1a0568@bvrithyderabad.edu.in>
2026-04-19 20:03:16 +0530

    Implement employee deletion from database

---
+++ DeleteEmp.java
@@ -0,0 +1,148 @@
+import java.sql.*;
+
+
+
+    public class DeleteEmp {
+
+        public static void main(String []args) throws SQLException {
+
+       try {
+
+         
+
+           DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+
+              
+
+            
+
+              Connection connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/24WH1A0568","root","1234");
+
+            
+
+        
+
+           
+
+      
+
+            Statement statement=connection.createStatement();
+
+            int nor = statement.executeUpdate("DELETE FROM employee WHERE emp_id = 3");
+
+            System.out.println("Rows deleted: " + nor);
+
+            ResultSet rs=statement.executeQuery("select * from employee");
+
+            
+
+            while(rs.next()) {
+
+            
+
+               System.out.println(
+
+                      rs.getInt("emp_id") + " | " +
+
+                      rs.getString("emp_name") + " | " +
+
+                      rs.getString("salary")
+
+                  );
+
+            }
+
+         }catch(SQLException e) {
+
+           e.printStackTrace();
+
+         }
+
+            
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+package pack;
+
+import java.sql.*;
+
+
+
+    public class DeleteEmp {
+
+        public static void main(String []args) throws SQLException {
+
+       try {
+
+         
+
+           DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+
+              
+
+            
+
+              Connection connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/24WH1A0568","root","1234");
+
+            
+
+        
+
+           
+
+      
+
+            Statement statement=connection.createStatement();
+
+            int nor = statement.executeUpdate("DELETE FROM employee WHERE emp_id = 3");
+
+            System.out.println("Rows deleted: " + nor);
+
+            ResultSet rs=statement.executeQuery("select * from employee");
+
+            
+
+            while(rs.next()) {
+
+            
+
+               System.out.println(
+
+                      rs.getInt("emp_id") + " | " +
+
+                      rs.getString("emp_name") + " | " +
+
+                      rs.getString("salary")
+
+                  );
+
+            }
+
+         }catch(SQLException e) {
+
+           e.printStackTrace();
+
+         }
+
+            
+
+        }
+
+    }
+
+
+
+
+
+
+
